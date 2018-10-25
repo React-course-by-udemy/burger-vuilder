53a440288bb9774c1708b57d349575b1e460ddc2
Adam Morsi <adam.morsi@yahoo.com>
2018-10-25 15:27:45 +0300

    End of section 18

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://burger-builder-e7728.firebaseio.com/'
+    baseURL: 'https://burger-builder-afb99.firebaseio.com/'
 });
 
 export default instance;
--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -30,7 +30,7 @@ export const fetchIngredientsFailed = () => {
 
 export const initIngredients = () => {
     return dispatch => {
-        axios.get('https://burger-builder-e7728.firebaseio.com/ingredients\n.json')
+        axios.get('https://burger-builder-afb99.firebaseio.com/ingredients.json')
             .then(response => {
                 dispatch(setIngredients(response.data));
             })
